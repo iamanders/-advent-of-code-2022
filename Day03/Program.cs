3117d44fd9ae6e176827c9065406fdf70c7d957c
iamanders <anders@iamanders.com>
2022-12-03 08:32:38 +0100

    Day 3

---
+++ Day03/Program.cs
@@ -0,0 +1,24 @@
+﻿int GetPrioritySum(IEnumerable<IEnumerable<char>> input) =>
+    input
+        .First()
+        .Where(c => input.All(line => line.Contains(c)))
+        .Select(c => CharValue(c))
+        .First();
+
+int CharValue(char c) =>
+    c < 'a' ? c - 'A' + 27 : c - 'a' + 1;
+
+var lines = File.ReadAllLines(@"./input-test.txt");
+
+var a = lines
+    .Select(line => line.Chunk(line.Length / 2))
+    .Select(GetPrioritySum)
+    .Sum();
+
+var b = lines
+    .Chunk(3)
+    .Select(GetPrioritySum)
+    .Sum();
+
+Console.WriteLine($"A: {a}");
+Console.WriteLine($"B: {b}");
