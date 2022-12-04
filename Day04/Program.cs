d2048eae581bb8d22ffb596914c503502b1a7a5f
iamanders <anders@iamanders.com>
2022-12-04 07:35:40 +0100

    Day 4 refactor

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -2,19 +2,20 @@
 
 var lines = File.ReadAllLines(@"./input-test.txt");
 
-var part1 = 0;
-var part2 = 0;
-foreach (var line in lines)
-{
-    var n = Regex.Matches(line, @"(\d)+");
-    var x1 = int.Parse(n[0].Value);
-    var y1 = int.Parse(n[1].Value);
-    var x2 = int.Parse(n[2].Value);
-    var y2 = int.Parse(n[3].Value);
-    
-    part1 += ((x1 <= x2 && y1 >= y2) || (x2 <= x1 && y2 >= y1)) ? 1 : 0;
-    part2 += x1 <= y2 && y1 >= x2 ? 1 : 0;
-}
+var data = lines
+    .Select(line => Regex.Matches(line, @"\d+"))
+    .Select(matches =>
+        new
+        {
+            x1 = int.Parse(matches[0].Value),
+            y1 = int.Parse(matches[1].Value),
+            x2 = int.Parse(matches[2].Value),
+            y2 = int.Parse(matches[3].Value),
+        })
+    .ToList();
+
+var part1 = data.Count(n => (n.x1 <= n.x2 && n.y1 >= n.y2) || (n.x2 <= n.x1 && n.y2 >= n.y1));
+var part2 = data.Count(n => n.x1 <= n.y2 && n.y1 >= n.x2);
 
 Console.WriteLine($"Part 1: {part1}");
 Console.WriteLine($"Part 2: {part2}");
