486184edebcb920c495e68e300762b638bb9adb1
iamanders <anders@iamanders.com>
2022-12-06 17:51:35 +0100

    Day 6

---
+++ Day06/Program.cs
@@ -0,0 +1,15 @@
+﻿var line = File.ReadAllText(@"./input-test.txt");
+
+int? FindMarker(int markerSize)
+{
+    for (var i = markerSize - 1; i < line.Length; i++)
+    {
+        var marker = line.Substring(i - (markerSize - 1), markerSize);
+        if (marker.Distinct().Count() >= markerSize)
+            return i + 1;
+    }
+    return null;
+}
+
+Console.WriteLine($"A: {FindMarker(4)}");
+Console.WriteLine($"B: {FindMarker(14)}");
