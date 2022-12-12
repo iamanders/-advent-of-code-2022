c8d43c6c7857d4e29b0f0c8354838ba108efa319
iamanders <anders@iamanders.com>
2022-12-12 17:05:45 +0100

    Day 10

---
+++ Day10/Program.cs
@@ -0,0 +1,34 @@
+﻿var lines = File.ReadAllLines(@"./input-test.txt");
+
+var cycleCount = 0;
+var signal = 0;
+var display = "";
+var x = 1;
+
+foreach (var line in lines)
+{
+    var xOriginal = x;
+    var commandCycles = line == "noop" ? 1 : 2;
+
+    if (line.StartsWith("addx"))
+    {
+        x += int.Parse(line[5..]);
+    }
+    
+    for (var i = 0; i < commandCycles; i++)
+    {
+        cycleCount++;
+        
+        var middle = new[] { xOriginal - 1, xOriginal, xOriginal + 1 };
+        display += middle.Contains((cycleCount - 1) % 40) ? "#" : " ";
+        display += ((cycleCount) % 40 == 0) ? '\n' : "";
+
+        if ((cycleCount - 20) % 40 == 0)
+        {
+            signal += cycleCount * xOriginal;
+        }
+    }
+}
+
+Console.WriteLine($"Part 1: {signal}");
+Console.WriteLine($"Part 2:\n {display}");
